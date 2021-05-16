2661e4371c23eaa957cfe394ade660a8d63a44e0
Guru Sai Teja Chinnakotla <84231319+SaitejzZ@users.noreply.github.com>
2021-05-16 20:12:20 +0530

    Update Question1.java

--- day1-assignment/src/Question1.java
+++ day1-assignment/src/Question1.java
@@ -6,6 +6,11 @@ public class Question1 {
 	 * 
 	 */
 	public static void main(String[] args) {
+		int x,y,sum;
+		x=Int.parseInt(ar[0]);
+		y=Int.parseInt(ar[1]);
+		sum=x+y;
+		System.out.println("sum");
 		
 	}
 }
